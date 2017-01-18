1fc74d48731b903f7bbac715c11b8b702c8646b6
Joey Fernau <Joey@Joeys-Air.wv.cc.cmu.edu>
2017-01-18 18:48:51 -0500

    Added asyncronous microphone reading. i.e. it will read microphone input whenever it hears it -- no need to poll to listen.  Renamed the speech_wrapper functions

--- smartMirror/speech_wrapper.py
+++ smartMirror/speech_wrapper.py
@@ -1,33 +1,51 @@
 # python3 -m pip install pocketsphinx
 
 from speech_recognition import Microphone, Recognizer, AudioData
+from requests import get
+import json
 
-# TODO: get audio snippet when audio detected
-def wait_for_audio():
-    with Microphone() as audio_source:
-        recognizer_instance = Recognizer()
-        recognizer_instance.energy_threshold = 1000
+def asynch_listener_fn(recognizer_instance, audio_data):
+    print("Performing Sphinx Speech to Text asynchronously")
+    try:
+        text_from_audio = \
+            recognizer_instance.recognize_sphinx( \
+                audio_data, show_all = False)
+    except Exception as e:
+        print(
+            "Exception in recognizer: " + str(e))
+        text_from_audio = None
 
-        print("Spawning reader")
-        audio_data = \
-            recognizer_instance.listen_in_background( \
-                audio_source, duration = duration)
+    print(text_from_audio)
 
-        print("Performing Sphinx Speech to Text")
-        try:
-            text_from_audio = \
-                recognizer_instance.recognize_sphinx( \
-                    audio_data, show_all = False)
-        except Exception as e:
-            print(
-                "Exception in recognizer: " + str(e))
-            text_from_audio = None
+    # TODO: send this text to Alina's API.AI using a GET request
+    some_structure = \
+        requests.get('http://api.aidomainnameWHATisTHIS/somemethod?query=%s' % text_from_audio)
+    some_structure.json() # is this a json?
+    return text_from_audio
 
-        print(text_from_audio)
-        return text_from_audio
-    return None
+def async_read_microphone():
+    recognizer_instance = Recognizer()
+    recognizer_instance.energy_threshold = 1000 # TODO: modify sensitivity
+    recognizer_instance.phrase_time_limit = 20 # TODO: I don't think this variable is working
+                                               #       find out how to change phrase time limit.
+                                               #       Do we want this phrase time_limit?
+    audio_source = Microphone()
+    print("Spawning reader")
+    stop_listener_fn = \
+        recognizer_instance.listen_in_background( \
+            audio_source, asynch_listener_fn)
 
-def read_microphone(duration = 5):
+    print("Waiting to read for 10 seconds")
+    from time import sleep
+    sleep(100)                  # TODO: when do we stop lisening?
+    stop_listener_fn()          #       Probably never; Maybe when we
+                                #       just quit out of app?
+    print("No longer taking audio input")
+    
+    return
+
+
+def sync_read_microphone(duration = 5):
     with Microphone() as audio_source:
         recognizer_instance = Recognizer()
         recognizer_instance.energy_threshold = 1000
