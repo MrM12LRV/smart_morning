b0a40443a38bc3458e7fe54f30f882f4109b5833
Joey Fernau <Joey@Joeys-Air.wv.cc.cmu.edu>
2017-01-20 09:41:38 -0500

    Separated SpeechFunctioner into its own file: speech_wrapper.py

--- smartMirror/SmartMirror.py
+++ smartMirror/SmartMirror.py
@@ -8,137 +8,15 @@ import json
 import requests                 # `python3 -m pip install requests`
 from requests import get
 
-from speech_recognition import Microphone, Recognizer, AudioData
+from speech_wrapper import SpeechFunctioner
 from PIL import Image, ImageTk  # `python3 -m pip install pillow`
 from os.path import join
-from pprint import PrettyPrinter
 
 #from instagram.client import InstagramAPI
 #import httplib2
 import sys
 
 BASE_PATH = "images"
-
-class SpeechFunctioner():
-
-    def __init__(self):
-        self.prettyprinter = PrettyPrinter(indent=4)
-
-        self.HIGH = 4000
-        self.LOW = 1000
-        self.SENSITIVITY = self.HIGH
-        self.response_json = None
-        self.voice_function = None
-
-
-    def __async_listener_fn(self, recognizer_instance, audio_data):
-
-        print("Performing Sphinx Speech to Text asynchronously")
-        try:
-            text_from_audio = \
-                recognizer_instance.recognize_sphinx( \
-                    audio_data, show_all = False)
-        except Exception as e:
-            print(
-                "Exception in recognizer: " + str(e))
-            text_from_audio = None
-
-        print("Text recognized:\n\t", end="")
-        print(text_from_audio)
-        print("Sending HTTP request to API AI")
-        self.response_json = \
-            get('https://api.api.ai/v1/query?v=20150910&query=%s&lang=en&sessionId=1234567890' % text_from_audio,
-                headers={
-                    'language-tag'  : 'en',
-                    'Authorization' : 'Bearer e4099166fd7a41218ba851d21e6866f5',
-                    'Content-Type'  : 'application/json; charset=utf-8'
-                }
-            )
-
-        print("Got structure back:")
-        try: self.prettyprinter.pprint(self.response_json.json())
-        except: print("Could not print JSON result.")
-
-        try:
-            self.voice_function = eval(self.response_json.json()['result']['metadata']['intentName'])
-        except Exception as err:
-            print("Not a valid voice function", self.voice_function)
-            print("Exception: {0}".format(err))
-            voice_function = None
-
-        return text_from_audio
-
-    # developer:    cac03a5b9aca49e2b63e97f7c0ae0cec    (managing entities and intents)
-    # client:       e4099166fd7a41218ba851d21e6866f5    (making queries)
-    # Authorization: Bearer YOUR_ACCESS_TOKEN
-
-    def async_read_microphone(self):
-        recognizer_instance = Recognizer()
-        recognizer_instance.energy_threshold = self.SENSITIVITY
-        recognizer_instance.phrase_time_limit = 20 # TODO: I don't think this variable is working
-                                                   #       find out how to change phrase time limit.
-                                                   #       Do we want this phrase time_limit?
-        audio_source = Microphone()
-        print("Spawning reader")
-        stop_listener_fn = \
-            recognizer_instance.listen_in_background( \
-                audio_source, self.__async_listener_fn)
-
-        print("Leaving read microphone")
-        """print("Waiting to read for 10 seconds")
-        from time import sleep
-        sleep(100)                  # TODO: when do we stop lisening?
-        stop_listener_fn()          #       Probably never; Maybe when we
-                                    #       just quit out of app?
-        print("No longer taking audio input")
-        
-        return
-        """
-
-
-    def sync_read_microphone(self, duration = 5):
-        with Microphone() as audio_source:
-            recognizer_instance = Recognizer()
-            recognizer_instance.energy_threshold = self.SENSITIVITY
-
-            print("Reading")
-            audio_data = \
-                recognizer_instance.record( \
-                    audio_source, duration = duration)
-
-            print("Performing Sphinx Speech to Text")
-            try:
-                text_from_audio = \
-                    recognizer_instance.recognize_sphinx( \
-                        audio_data, show_all = False)
-            except Exception as e:
-                print(
-                    "Exception in recognizer: " + str(e))
-                text_from_audio = None
-
-            print(text_from_audio)
-            print("Sending HTTP request to API AI")
-
-            self.response_json = \
-                get('https://api.api.ai/v1/query?v=20150910&query=%s&lang=en&sessionId=1234567890' % text_from_audio,
-                    headers={
-                        'language-tag'  : 'en',
-                        'Authorization' : 'Bearer e4099166fd7a41218ba851d21e6866f5',
-                        'Content-Type'  : 'application/json; charset=utf-8'
-                    }
-                )
-            print("Got structure back:")
-            try: self.prettyprinter.pprint(self.response_json.json())
-            except: print("Could not print JSON result.")
-
-            try:
-                self.voice_function = \
-                    eval(self.response_json.json()['result']['metadata']['intentName'])
-            except Exception as err:
-                print("Not a valid voice function", self.voice_function)
-                print("Exception: {0}".format(err))
-                self.voice_function = None
-        return None
 
 ##### Write functions for voice commands
 
@@ -355,12 +233,24 @@ class SmartMirror(object):
         self.bgColor = "black"
         self.isDrawHearts = False
     
-    def timerFired(self):
-        
+    def __call_voice_command(self):
         print("Voice function is", self.sf.voice_function)
+
+        if self.sf.voice_function is not None:
+            try:
+                self.sf.voice_function = eval(self.sf.voice_function)
+
+            except Exception as err:
+                print("Not a valid voice function", self.sf.voice_function)
+                print("Exception: {0}".format(err))
+                self.sf.voice_function = None
+
         if self.sf.voice_function is not None and callable(self.sf.voice_function):
             self.sf.voice_function()
             self.sf.voice_function = None # reset the function
+
+    def timerFired(self):
+        self.__call_voice_command()
         
         self.timeDate.update()
         self.news.article += 0.04
--- smartMirror/speech_wrapper.py
+++ smartMirror/speech_wrapper.py
@@ -3,98 +3,23 @@
 from speech_recognition import Microphone, Recognizer, AudioData
 from requests import get
 import json
-import pprint
+from pprint import PrettyPrinter
 
-prettyprinter = pprint.PrettyPrinter(indent=4)
+class SpeechFunctioner():
 
-HIGH = 4000
-LOW = 1000
-SENSITIVITY = HIGH
-some_structure = None
-voice_function = None
+    def __init__(self):
+        self.prettyprinter = PrettyPrinter(indent=4)
 
-
-
-
-
-def asynch_listener_fn(recognizer_instance, audio_data):
-    global some_structure, voice_function, prettyprinter
-
-    print("Performing Sphinx Speech to Text asynchronously")
-    try:
-        text_from_audio = \
-            recognizer_instance.recognize_sphinx( \
-                audio_data, show_all = False)
-    except Exception as e:
-        print(
-            "Exception in recognizer: " + str(e))
-        text_from_audio = None
-
-    print("Text recognized:\n\t", end="")
-    print(text_from_audio)
-    print("Sending HTTP request to API AI")
-    some_structure = \
-        get('https://api.api.ai/v1/query?v=20150910&query=%s&lang=en&sessionId=1234567890' % text_from_audio,
-            headers={
-                'language-tag'  : 'en',
-                'Authorization' : 'Bearer e4099166fd7a41218ba851d21e6866f5',
-                'Content-Type'  : 'application/json; charset=utf-8'
-            }
-        )
-
-    print("Got structure back:")
-    try: prettyprinter.pprint(some_structure.json())
-    except: print("Could not print JSON result.")
-
-    try:
-        voice_function = eval(some_structure.json()['result']['metadata']['intentName'])
-    except Exception as err:
-        print("Not a valid voice function", voice_function)
-        print("Exception: {0}".format(err))
-        voice_function = None
-
-    return text_from_audio
-
-# developer:    cac03a5b9aca49e2b63e97f7c0ae0cec    (managing entities and intents)
-# client:       e4099166fd7a41218ba851d21e6866f5    (making queries)
-# Authorization: Bearer YOUR_ACCESS_TOKEN
-
-def async_read_microphone():
-    recognizer_instance = Recognizer()
-    recognizer_instance.energy_threshold = SENSITIVITY # TODO: modify sensitivity
-    recognizer_instance.phrase_time_limit = 20 # TODO: I don't think this variable is working
-                                               #       find out how to change phrase time limit.
-                                               #       Do we want this phrase time_limit?
-    audio_source = Microphone()
-    print("Spawning reader")
-    stop_listener_fn = \
-        recognizer_instance.listen_in_background( \
-            audio_source, asynch_listener_fn)
-
-    print("Leaving read microphone")
-    """print("Waiting to read for 10 seconds")
-    from time import sleep
-    sleep(100)                  # TODO: when do we stop lisening?
-    stop_listener_fn()          #       Probably never; Maybe when we
-                                #       just quit out of app?
-    print("No longer taking audio input")
-    
-    return
-    """
+        self.HIGH = 4000
+        self.LOW = 1000
+        self.SENSITIVITY = self.HIGH
+        self.response_json = None
+        self.voice_function = None
 
 
-def sync_read_microphone(duration = 5):
-    global some_structure, voice_function
-    with Microphone() as audio_source:
-        recognizer_instance = Recognizer()
-        recognizer_instance.energy_threshold = SENSITIVITY
+    def __async_listener_fn(self, recognizer_instance, audio_data):
 
-        print("Reading")
-        audio_data = \
-            recognizer_instance.record( \
-                audio_source, duration = duration)
-
-        print("Performing Sphinx Speech to Text")
+        print("Performing Sphinx Speech to Text asynchronously")
         try:
             text_from_audio = \
                 recognizer_instance.recognize_sphinx( \
@@ -104,10 +29,10 @@ def sync_read_microphone(duration = 5):
                 "Exception in recognizer: " + str(e))
             text_from_audio = None
 
+        print("Text recognized:\n\t", end="")
         print(text_from_audio)
         print("Sending HTTP request to API AI")
-        # TODO: send this text to Alina's API.AI using a GET request
-        some_structure = \
+        self.response_json = \
             get('https://api.api.ai/v1/query?v=20150910&query=%s&lang=en&sessionId=1234567890' % text_from_audio,
                 headers={
                     'language-tag'  : 'en',
@@ -115,16 +40,88 @@ def sync_read_microphone(duration = 5):
                     'Content-Type'  : 'application/json; charset=utf-8'
                 }
             )
+
         print("Got structure back:")
-        try: prettyprinter.pprint(some_structure.json())
+        try: self.prettyprinter.pprint(self.response_json.json())
         except: print("Could not print JSON result.")
 
         try:
-            voice_function = eval(some_structure.json()['result']['metadata']['intentName'])
-            if not callable(voice_function):
-                assert(False)
+            self.voice_function = self.response_json.json()['result']['metadata']['intentName']
         except Exception as err:
-            print("Not a valid voice function", voice_function)
+            print("Not a valid voice function", self.voice_function)
             print("Exception: {0}".format(err))
             voice_function = None
-    return None
+
+        return text_from_audio
+
+    # developer:    cac03a5b9aca49e2b63e97f7c0ae0cec    (managing entities and intents)
+    # client:       e4099166fd7a41218ba851d21e6866f5    (making queries)
+    # Authorization: Bearer YOUR_ACCESS_TOKEN
+
+    def async_read_microphone(self):
+        recognizer_instance = Recognizer()
+        recognizer_instance.energy_threshold = self.SENSITIVITY
+        recognizer_instance.phrase_time_limit = 20 # TODO: I don't think this variable is working
+                                                   #       find out how to change phrase time limit.
+                                                   #       Do we want this phrase time_limit?
+        audio_source = Microphone()
+        print("Spawning reader")
+        stop_listener_fn = \
+            recognizer_instance.listen_in_background( \
+                audio_source, self.__async_listener_fn)
+
+        print("Leaving read microphone")
+        """print("Waiting to read for 10 seconds")
+        from time import sleep
+        sleep(100)                  # TODO: when do we stop lisening?
+        stop_listener_fn()          #       Probably never; Maybe when we
+                                    #       just quit out of app?
+        print("No longer taking audio input")
+        
+        return
+        """
+
+
+    def sync_read_microphone(self, duration = 5):
+        with Microphone() as audio_source:
+            recognizer_instance = Recognizer()
+            recognizer_instance.energy_threshold = self.SENSITIVITY
+
+            print("Reading")
+            audio_data = \
+                recognizer_instance.record( \
+                    audio_source, duration = duration)
+
+            print("Performing Sphinx Speech to Text")
+            try:
+                text_from_audio = \
+                    recognizer_instance.recognize_sphinx( \
+                        audio_data, show_all = False)
+            except Exception as e:
+                print(
+                    "Exception in recognizer: " + str(e))
+                text_from_audio = None
+
+            print(text_from_audio)
+            print("Sending HTTP request to API AI")
+
+            self.response_json = \
+                get('https://api.api.ai/v1/query?v=20150910&query=%s&lang=en&sessionId=1234567890' % text_from_audio,
+                    headers={
+                        'language-tag'  : 'en',
+                        'Authorization' : 'Bearer e4099166fd7a41218ba851d21e6866f5',
+                        'Content-Type'  : 'application/json; charset=utf-8'
+                    }
+                )
+            print("Got structure back:")
+            try: self.prettyprinter.pprint(self.response_json.json())
+            except: print("Could not print JSON result.")
+
+            try:
+                self.voice_function = \
+                    self.response_json.json()['result']['metadata']['intentName']
+            except Exception as err:
+                print("Not a valid voice function", self.voice_function)
+                print("Exception: {0}".format(err))
+                self.voice_function = None
+        return None
